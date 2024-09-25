73d291e612ef885077e6c59ff5a5e29a91781f42
Ahmad Rizal Fahlefi <51229486+ahmad-rizal-fahlefi@users.noreply.github.com>
2024-09-25 20:41:20 +0700

    Update tes.php

--- tes.php
+++ tes.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tes hubung git</title>
+    <title>CEK BUAT KONFLIK</title>
 </head>
 <body>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, modi. Quisquam, architecto tenetur nihil dolores nam eaque pariatur labore voluptatum odio, deserunt, magnam quam facilis repellendus alias voluptatibus in omnis.</p>
