b3cb47b219cbbddb6301b857f8cc0544824e74ca
ahmad-rizal-fahlefi <ahmadrizalf536@gmail.com>
2024-09-25 15:03:11 +0700

    coba hubung 2

--- tes.php
+++ tes.php
@@ -6,6 +6,6 @@
     <title>Tes hubung git</title>
 </head>
 <body>
-    
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, modi. Quisquam, architecto tenetur nihil dolores nam eaque pariatur labore voluptatum odio, deserunt, magnam quam facilis repellendus alias voluptatibus in omnis.</p>
 </body>
 </html>
